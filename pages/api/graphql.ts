20dce33057039fd08a22d8d4e2e3ca3b5c0448af
Yasin Khan <yasinkhan500@gmail.com>
2022-08-26 13:47:57 -0700

    test15

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -15,20 +15,29 @@ const startServer = apolloServer.start();
 
 const cors = Cors({
 	// origin: 'https://animedia.vercel.app',
-	allowCredentials: true,
+	// allowCredentials: true,
 });
 
 export default cors(async function handler(req, res) {
+	// res.setHeader('Access-Control-Allow-Credentials', 'true');
+	// res.setHeader(
+	// 	'Access-Control-Allow-Origin',
+	// 	'https://studio.apollographql.com'
+	// );
+	// res.setHeader(
+	// 	'Access-Control-Allow-Headers',
+	// 	'Origin, X-Requested-With, Content-Type, Accept'
+	// );
 	res.setHeader('Access-Control-Allow-Credentials', 'true');
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader(
-		'Access-Control-Allow-Origin',
-		'https://studio.apollographql.com'
+		'Access-Control-Allow-Methods',
+		'GET,OPTIONS,PATCH,DELETE,POST,PUT'
 	);
 	res.setHeader(
 		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept'
+		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
 	);
-	// res.setHeader('Access-Control-Allow-Origin', 'https://animedia.vercel.app');
 
 	if (req.method === 'OPTIONS') {
 		res.end();
