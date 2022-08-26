a8b3ca5bdb8205ecac27af807ce44e88a7c410f2
Yasin Khan <yasinkhan500@gmail.com>
2022-08-26 13:25:29 -0700

    test11

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,19 +1,71 @@
+// import { ApolloServer } from 'apollo-server-micro';
+// import { PageConfig } from 'next';
+// import { schema } from '../../graphql/schema';
+// import { context } from '../../graphql/context';
+// import Cors from 'micro-cors';
+
+// const apolloServer = new ApolloServer({
+// 	context,
+// 	schema,
+// });
+
+// const startServer = apolloServer.start();
+
+// const cors = Cors({
+// 	// origin: 'https://animedia.vercel.app',
+// 	// allowCredentials: true,
+// });
+
+// export default cors(async function handler(req, res) {
+// 	res.setHeader('Access-Control-Allow-Credentials', 'true');
+// 	res.setHeader(
+// 		'Access-Control-Allow-Origin',
+// 		'https://studio.apollographql.com'
+// 	);
+// 	res.setHeader(
+// 		'Access-Control-Allow-Headers',
+// 		'Origin, X-Requested-With, Content-Type, Accept'
+// 	);
+// 	// res.setHeader('Access-Control-Allow-Origin', 'https://animedia.vercel.app');
+
+// 	if (req.method === 'OPTIONS') {
+// 		res.end();
+// 		return false;
+// 	}
+
+// 	await startServer;
+
+// 	await apolloServer.createHandler({
+// 		path: '/api/graphql',
+// 	})(req, res);
+// });
+
+// export const config: PageConfig = {
+// 	api: {
+// 		bodyParser: false,
+// 	},
+// };
+
 import { ApolloServer } from 'apollo-server-micro';
 import { PageConfig } from 'next';
 import { schema } from '../../graphql/schema';
 import { context } from '../../graphql/context';
 import Cors from 'micro-cors';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 const apolloServer = new ApolloServer({
 	context,
 	schema,
+	csrfPrevention: true,
+	cache: 'bounded',
+	plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
 });
 
 const startServer = apolloServer.start();
 
 const cors = Cors({
 	// origin: 'https://animedia.vercel.app',
-	allowCredentials: true,
+	// allowCredentials: true,
 });
 
 export default cors(async function handler(req, res) {
