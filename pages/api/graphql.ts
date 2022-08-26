d3047e103927596b88409b2971c5fe6f86b4b311
Yasin Khan <yasinkhan500@gmail.com>
2022-08-26 13:53:55 -0700

    test15

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -29,14 +29,17 @@ export default cors(async function handler(req, res) {
 	// 	'Origin, X-Requested-With, Content-Type, Accept'
 	// );
 	res.setHeader('Access-Control-Allow-Credentials', 'true');
-	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Origin',
+		'https://studio.apollographql.com'
+	);
 	res.setHeader(
 		'Access-Control-Allow-Methods',
 		'GET,OPTIONS,PATCH,DELETE,POST,PUT'
 	);
 	res.setHeader(
 		'Access-Control-Allow-Headers',
-		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+		'Origin, X-Requested-With, Content-Type, Accept'
 	);
 
 	if (req.method === 'OPTIONS') {
