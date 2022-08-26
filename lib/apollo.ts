6aad0aba03899c97d7a1054bee55920b9203e3a6
Yasin Khan <yasinkhan500@gmail.com>
2022-08-26 15:30:00 -0700

    test17

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,17 +1,47 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink, from } from '@apollo/client';
 import { relayStylePagination } from '@apollo/client/utilities';
+import { onError } from '@apollo/client/link/error';
 
-export const client = new ApolloClient({
-	// uri: 'http://localhost:3000/api/graphql',
-	uri: 'https://animedia.vercel.app/api/graphql',
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+	if (graphQLErrors)
+		graphQLErrors.forEach(err => {
+			const { locations, message, path } = err;
+			console.warn(
+				`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+			);
+		});
+});
 
-	cache: new InMemoryCache({
-		typePolicies: {
-			Query: {
-				fields: {
-					links: relayStylePagination(),
-				},
-			},
+const link = from([
+	errorLink,
+	new HttpLink({
+		uri:
+			process.env.NODE_ENV === 'production'
+				? 'https://animedia.vercel.app/api/graphql'
+				: 'http://localhost:3000/api/graphql',
+		credentials: 'include',
+		headers: {
+			Origin: 'https://animedia.vercel.app', // <- Added this and now builds are no longer 500 erroring on vercel
 		},
 	}),
+]);
+
+export const client = new ApolloClient({
+	cache: new InMemoryCache(),
+	link: link,
 });
+
+// export const client = new ApolloClient({
+// 	uri: 'http://localhost:3000/api/graphql',
+// 	// uri: 'https://animedia.vercel.app/api/graphql',
+
+// 	cache: new InMemoryCache({
+// 		typePolicies: {
+// 			Query: {
+// 				fields: {
+// 					links: relayStylePagination(),
+// 				},
+// 			},
+// 		},
+// 	}),
+// });
