fa1ce9fc7a049f8de28f411fe1723400d0cbe8b4
Yasin Khan <yasinkhan500@gmail.com>
2022-08-29 19:51:25 -0700

    created search page

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useRouter } from 'next/router';
 
-interface Props {}
+const SearchBar = () => {
+	const router = useRouter();
+	const searchRef = useRef<HTMLInputElement>(null);
 
-const SearchBar = (props: Props) => {
+	const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+		router.push(`/search?q=${searchRef.current?.value.split(' ').join('+')}`);
+	};
+
 	return (
 		<form className='flex justify-center w-full relative'>
 			<input
 				className='w-[98%] text-base py-4 pl-[3rem] pr-4 bg-gray-300 text-gray-700 rounded-md border-none transition duration-300 focus:border-0 focus:outline-none focus:shadow-[0_1px_12px_#b8c6db]'
 				type='text'
 				placeholder='Search for a movie, tv show, or person...'
+				ref={searchRef}
 			/>
 			<button
+				onClick={handleSubmit}
 				className='absolute bg-transparent text-[1.5rem] text-gray-700 z-10 border-none transition-all hover:cursor-pointer hover:text-black hover:scale-125 focus:text-black focus:outline-none left-[calc(100%-3rem)] top-2'
-				type='button'
+				type='submit'
 			>
 				<i className='fas fa-search'></i>
 			</button>
---
+++ pages/search.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import React from 'react';
+
+interface Props {}
+
+const Search: NextPage = (props: Props) => {
+	return (
+		<div className='mt-[calc(var(--header-height-mobile)+1rem)]'>search</div>
+	);
+};
+
+export default Search;
