6f1a4fe9ea4456756253e232a2d6baaf4119eed1
Yasin Khan <yasinkhan500@gmail.com>
2022-09-02 16:56:50 -0700

    made css base for dropdown windows

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,15 +5,51 @@ const Header = () => {
 	const router = useRouter();
 	return (
 		<header className='fixed top-0 flex items-center w-full h-[var(--header-height-mobile)] z-[999] font-[Rubik] bg-slate-300'>
-			<nav className='flex w-full ml-[4rem]'>
+			<nav className='flex items-center w-full ml-[4rem]'>
 				<section onClick={() => router.push('/')} className='cursor-pointer'>
 					<p>HOME</p>
 				</section>
-				<section className='flex w-full justify-between ml-[4rem]'>
+				<section className='flex w-full justify-between ml-[4rem] items-center'>
 					<ul id='left-section' className='flex justify-around w-[50%]'>
-						<li>Movies</li>
-						<li>TV Shows</li>
-						<li>People</li>
+						<li className='group relative inline-block hover:block'>
+							<p className='text-black p-3 m-0 no-underline block hover:bg-slate-500 rounded'>
+								Movies
+							</p>
+							<div className='hidden absolute bg-red-500 min-w-[160px] rounded shadow z-[1] group-hover:block'>
+								<p className='text-black p-3 m-0 no-underline block hover:bg-slate-500 rounded'>
+									Popular Movies
+								</p>
+								<p className='text-black p-3 m-0 no-underline block hover:bg-slate-500 rounded'>
+									Top Rated
+								</p>
+							</div>
+						</li>
+						<li className='group relative inline-block hover:block'>
+							<p className='text-black p-3 m-0 no-underline block hover:bg-slate-500 rounded'>
+								TV Shows
+							</p>
+							<div className='hidden absolute bg-red-500 min-w-[160px] rounded shadow z-[1] group-hover:block'>
+								<p className='text-black p-3 m-0 no-underline block hover:bg-slate-500 rounded'>
+									Most Popular
+								</p>
+								<p className='text-black p-3 m-0 no-underline block hover:bg-slate-500 rounded'>
+									Top Rated
+								</p>
+							</div>
+						</li>
+						<li className='group relative inline-block hover:block'>
+							<p className='text-black p-3 m-0 no-underline block hover:bg-slate-500 rounded'>
+								People
+							</p>
+							<div className='hidden absolute bg-red-500 min-w-[160px] rounded shadow z-[1] group-hover:block'>
+								<p className='text-black p-3 m-0 no-underline block hover:bg-slate-500 rounded'>
+									Most Popular
+								</p>
+								<p className='text-black p-3 m-0 no-underline block hover:bg-slate-500 rounded'>
+									Top Rated
+								</p>
+							</div>
+						</li>
 					</ul>
 					<ul id='right-section' className='flex justify-around w-[15%]'>
 						<li>EN</li>
