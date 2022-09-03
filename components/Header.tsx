0a92b6bb2a2b722435ace577ff27ba5ccb1e68af
Yasin Khan <yasinkhan500@gmail.com>
2022-09-02 20:47:46 -0700

    stache

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,9 +12,7 @@ const Header = () => {
 				<section className='flex w-full justify-between ml-[4rem] items-center'>
 					<ul id='left-section' className='flex justify-around w-[50%]'>
 						<li className='group relative inline-block hover:block'>
-							<p className='text-black p-3 m-0 no-underline block hover:bg-slate-500 rounded'>
-								Movies
-							</p>
+							<p className='text-black p-3 m-0 no-underline block'>Movies</p>
 							<div className='hidden absolute bg-red-500 min-w-[160px] rounded shadow z-[1] group-hover:block'>
 								<p className='text-black p-3 m-0 no-underline block hover:bg-slate-500 rounded'>
 									Popular Movies
@@ -25,9 +23,7 @@ const Header = () => {
 							</div>
 						</li>
 						<li className='group relative inline-block hover:block'>
-							<p className='text-black p-3 m-0 no-underline block hover:bg-slate-500 rounded'>
-								TV Shows
-							</p>
+							<p className='text-black p-3 m-0 no-underline block'>TV Shows</p>
 							<div className='hidden absolute bg-red-500 min-w-[160px] rounded shadow z-[1] group-hover:block'>
 								<p className='text-black p-3 m-0 no-underline block hover:bg-slate-500 rounded'>
 									Most Popular
@@ -38,9 +34,7 @@ const Header = () => {
 							</div>
 						</li>
 						<li className='group relative inline-block hover:block'>
-							<p className='text-black p-3 m-0 no-underline block hover:bg-slate-500 rounded'>
-								People
-							</p>
+							<p className='text-black p-3 m-0 no-underline block'>People</p>
 							<div className='hidden absolute bg-red-500 min-w-[160px] rounded shadow z-[1] group-hover:block'>
 								<p className='text-black p-3 m-0 no-underline block hover:bg-slate-500 rounded'>
 									Most Popular
