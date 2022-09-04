be5c0049e394195f5cd4483a1a724fde0a4354b0
Yasin Khan <yasinkhan500@gmail.com>
2022-09-04 14:03:25 -0700

    finished pagination

--- pages/people/popular.tsx
+++ pages/people/popular.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { IUseGetQuery } from '@ts/interfaces';
 import {
 	NexusGenObjects,
@@ -7,23 +7,67 @@ import {
 import { useGetQuery } from '../../hooks/useGetQuery';
 import * as Queries from '../../graphql/queries';
 import PeopleList from 'components/PeopleList';
+import Pagination from 'components/Pagination';
 
 const PopularPeople = () => {
-	const [pageNum, setPageNum] = useState(1);
+	const [_currPeopleItems, setCurrPeopleItems] = useState<
+		NexusGenObjects['PeopleRes']['results']
+	>([]);
+	const [currPage, setCurrPage] = useState(1);
+	const [peopleItemsPerPage] = useState(20);
+
 	const {
 		data: popularPeopleData,
 	}: IUseGetQuery<NexusGenObjects['PeopleRes']> = useGetQuery<
 		NexusGenArgTypes['Query']['popularPeople']
 	>(Queries.QUERY_POPULAR_PEOPLE, {
-		page: pageNum,
+		page: currPage,
 	});
+
+	useEffect(() => {
+		if (popularPeopleData) {
+			const endIdx = currPage * peopleItemsPerPage;
+			const startIdx = endIdx - peopleItemsPerPage;
+			const peopleItemsCopy = [...popularPeopleData.results];
+			setCurrPeopleItems(peopleItemsCopy.slice(startIdx, endIdx));
+		}
+	}, [currPage, popularPeopleData, peopleItemsPerPage]);
+
+	const goToNextPage = () => {
+		setCurrPage(currPage => currPage + 1);
+	};
+
+	const goToPrevPage = () => {
+		setCurrPage(currPage => currPage - 1);
+	};
+
+	const getPaginationGroup = () => {
+		let start =
+			Math.floor((currPage - 1) / peopleItemsPerPage) * peopleItemsPerPage;
+		return new Array(peopleItemsPerPage)
+			.fill(null)
+			.map((_, idx) => start + idx + 1);
+	};
 
 	console.log(popularPeopleData);
 
 	return (
 		<section className='mt-[calc(var(--header-height-mobile)+1rem)]'>
 			popular people
-			{popularPeopleData && <PeopleList peopleData={popularPeopleData} />}
+			{popularPeopleData && (
+				<>
+					<PeopleList peopleData={popularPeopleData} />
+					<Pagination
+						itemsPerPage={peopleItemsPerPage}
+						totalItems={popularPeopleData.total_results}
+						currPage={currPage}
+						pageNums={getPaginationGroup()}
+						paginate={pageNum => setCurrPage(pageNum)}
+						goToPrevPage={goToPrevPage}
+						goToNextPage={goToNextPage}
+					/>
+				</>
+			)}
 		</section>
 	);
 };
