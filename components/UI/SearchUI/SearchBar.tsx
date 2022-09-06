ee3e8e5019d7d11308e0206aa452ef318c3bc395
Yasin Khan <yasinkhan500@gmail.com>
2022-09-06 16:23:53 -0700

    stache

--- components/UI/SearchUI/SearchBar.tsx
+++ components/UI/SearchUI/SearchBar.tsx
@@ -5,7 +5,7 @@ const SearchBar = () => {
 	const router = useRouter();
 	const searchRef = useRef<HTMLInputElement>(null);
 
-	const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		if (searchRef.current?.value.trim().length === 0) return;
 
@@ -15,7 +15,10 @@ const SearchBar = () => {
 	};
 
 	return (
-		<form className='flex justify-center w-full relative'>
+		<form
+			className='flex justify-center w-full relative'
+			onSubmit={handleSubmit}
+		>
 			<input
 				className='w-[98%] text-base py-4 pl-[3rem] pr-4 bg-gray-300 text-gray-700 rounded-md border-none transition duration-300 focus:border-0 focus:outline-none focus:shadow-[0_1px_12px_#b8c6db]'
 				type='text'
@@ -23,7 +26,6 @@ const SearchBar = () => {
 				ref={searchRef}
 			/>
 			<button
-				onClick={handleSubmit}
 				className='absolute bg-transparent text-[1.5rem] text-gray-700 z-10 border-none transition-all hover:cursor-pointer hover:text-black hover:scale-125 focus:text-black focus:outline-none left-[calc(100%-3rem)] top-2'
 				type='submit'
 			>
