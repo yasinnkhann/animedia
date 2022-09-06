2b875dcd5c9f10c39554098dc6299fd40920fd05
Yasin Khan <yasinkhan500@gmail.com>
2022-09-06 16:50:58 -0700

    stache

--- components/MediaCard.tsx
+++ components/MediaCard.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import { NexusGenObjects } from '../graphql/generated/nexus-typegen';
+import { getDetailsPageRoute } from '../utils/getDetailsPageRoute';
+import { ESearchType } from '@ts/enums';
+import { CLIENT_BASE_URL } from '../utils/URLs';
 
 interface Props {
 	media: NexusGenObjects['MovieResult'] | NexusGenObjects['ShowResult'];
 }
 
 const MediaCard = ({ media }: Props) => {
+	const router = useRouter();
+
 	const mediaTitle = 'title' in media ? media.title : media.name;
+
+	const handleGoToDetailsPage = () => {
+		let mediaType: ESearchType.MOVIE | ESearchType.SHOW;
+
+		mediaType = 'title' in media ? ESearchType.MOVIE : ESearchType.SHOW;
+
+		router.replace(
+			`${CLIENT_BASE_URL}/${getDetailsPageRoute(
+				mediaType,
+				media.id,
+				mediaTitle
+			)}`
+		);
+	};
 
 	return (
 		<div>
-			<h1>{mediaTitle}</h1>
+			<h1 onClick={handleGoToDetailsPage}>{mediaTitle}</h1>
 		</div>
 	);
 };
--- utils/URLs.ts
+++ utils/URLs.ts
@@ -1,2 +1,6 @@
 export const BASE_URL = 'https://api.themoviedb.org/3';
 export const BASE_IMG_URL = 'https://image.tmdb.org/t/p/original';
+export const CLIENT_BASE_URL =
+	process.env.NODE_ENV === 'production'
+		? 'https://animedia.vercel.app'
+		: 'http://localhost:3000';
