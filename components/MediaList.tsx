aeadc6a5039ec1965e89cc0c7acc57262162787c
Yasin Khan <yasinkhan500@gmail.com>
2022-09-28 23:26:39 -0700

    tidied up table

--- components/MediaList.tsx
+++ components/MediaList.tsx
@@ -15,44 +15,50 @@ const MediaList = ({ mediaData, pageNum }: Props) => {
 	const { data: session } = useSession();
 
 	return (
-		<table className='w-full'>
-			<thead>
-				<tr>
-					<th>Rank</th>
-					<th>Title</th>
-					<th>Rating</th>
-					{session && (
-						<>
-							<th>Your Rating</th>
-							<th>Status</th>
-						</>
-					)}
-				</tr>
-			</thead>
+		<section className='px-44'>
+			<table className='w-full'>
+				<thead>
+					<tr className='border-4 border-blue-300'>
+						<th className='border-r-4 border-blue-300'>Rank</th>
+						<th className='border-r-4 border-blue-300'>Title</th>
+						<th>Rating</th>
+						{session && (
+							<>
+								<th className='border-x-4 border-blue-300'>Your Rating</th>
+								<th>Status</th>
+							</>
+						)}
+					</tr>
+				</thead>
 
-			<tbody>
-				{mediaData.results.map((media, idx) => {
-					let mediaComp: JSX.Element;
+				<tbody className='border-b-4 border-blue-300'>
+					{mediaData.results.map((media, idx) => {
+						let mediaComp: JSX.Element;
 
-					if ('title' in media) {
-						mediaComp = (
-							<MovieCard
-								movie={media}
-								rank={pageNum * RESULTS_PER_PAGE - (RESULTS_PER_PAGE - idx) + 1}
-							/>
-						);
-					} else {
-						mediaComp = (
-							<ShowCard
-								show={media}
-								rank={pageNum * RESULTS_PER_PAGE - (RESULTS_PER_PAGE - idx) + 1}
-							/>
-						);
-					}
-					return <Fragment key={media.id}>{mediaComp}</Fragment>;
-				})}
-			</tbody>
-		</table>
+						if ('title' in media) {
+							mediaComp = (
+								<MovieCard
+									movie={media}
+									rank={
+										pageNum * RESULTS_PER_PAGE - (RESULTS_PER_PAGE - idx) + 1
+									}
+								/>
+							);
+						} else {
+							mediaComp = (
+								<ShowCard
+									show={media}
+									rank={
+										pageNum * RESULTS_PER_PAGE - (RESULTS_PER_PAGE - idx) + 1
+									}
+								/>
+							);
+						}
+						return <Fragment key={media.id}>{mediaComp}</Fragment>;
+					})}
+				</tbody>
+			</table>
+		</section>
 	);
 };
 
