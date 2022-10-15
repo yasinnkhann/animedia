bcafcc634b931f330fc26a07a063abe87fa3b1ca
Yasin Khan <yasinkhan500@gmail.com>
2022-10-15 14:15:55 -0700

    styled my shows and my movies ish

--- pages/my-shows/index.tsx
+++ pages/my-shows/index.tsx
@@ -13,51 +13,91 @@ const MyShows: NextPage = () => {
 	const { data: usersShowsData }: IUseGQLQuery<NexusGenObjects['UserShow'][]> =
 		useGQLQuery(Queries.QUERY_GET_USERS_SHOWS);
 
-	console.log('USERS SHOWS DATA: ', usersShowsData);
-
 	return (
 		<main className='mt-[calc(var(--header-height-mobile)+1rem)]'>
-			<h1>My Shows</h1>
-			<section>
-				<div>
-					<a onClick={() => router.push('/my-shows/watching')}>Watching:</a>
-					<span>
-						{usersShowsData?.filter(show => show.status === 'WATCHING').length}
-					</span>
-				</div>
+			<section className='flex justify-center'>
+				<section className='w-[30rem] h-[30rem] mt-36'>
+					<h1 className='text-center mb-4'>My Shows</h1>
+					<section className='flex justify-center items-center'>
+						<section className='bg-gray-200 flex flex-col justify-around items-center rounded-lg w-[25rem] h-[15rem]'>
+							<div className='pt-4'>
+								<a
+									className='no-underline'
+									onClick={() => router.push('/my-shows/watching')}
+								>
+									Watching:{' '}
+								</a>
+								<span>
+									{
+										usersShowsData?.filter(show => show.status === 'WATCHING')
+											.length
+									}
+								</span>
+							</div>
 
-				<div>
-					<a onClick={() => router.push('/my-shows/completed')}>Completed:</a>
-					<span>
-						{usersShowsData?.filter(show => show.status === 'COMPLETED').length}
-					</span>
-				</div>
+							<div>
+								<a
+									className='no-underline'
+									onClick={() => router.push('/my-shows/completed')}
+								>
+									Completed:{' '}
+								</a>
+								<span>
+									{
+										usersShowsData?.filter(show => show.status === 'COMPLETED')
+											.length
+									}
+								</span>
+							</div>
 
-				<div>
-					<a onClick={() => router.push('/my-shows/on-hold')}>On-Hold:</a>
-					<span>
-						{usersShowsData?.filter(show => show.status === 'ON_HOLD').length}
-					</span>
-				</div>
+							<div>
+								<a
+									className='no-underline'
+									onClick={() => router.push('/my-shows/on-hold')}
+								>
+									On-Hold:{' '}
+								</a>
+								<span>
+									{
+										usersShowsData?.filter(show => show.status === 'ON_HOLD')
+											.length
+									}
+								</span>
+							</div>
 
-				<div>
-					<a onClick={() => router.push('/my-shows/dropped')}>Dropped:</a>
-					<span>
-						{usersShowsData?.filter(show => show.status === 'DROPPED').length}
-					</span>
-				</div>
+							<div>
+								<a
+									className='no-underline'
+									onClick={() => router.push('/my-shows/dropped')}
+								>
+									Dropped:{' '}
+								</a>
+								<span>
+									{
+										usersShowsData?.filter(show => show.status === 'DROPPED')
+											.length
+									}
+								</span>
+							</div>
 
-				<div>
-					<a onClick={() => router.push('/my-shows/plan-to-watch')}>
-						Plan to Watch:
-					</a>
-					<span>
-						{
-							usersShowsData?.filter(show => show.status === 'PLAN_TO_WATCH')
-								.length
-						}
-					</span>
-				</div>
+							<div className='pb-4'>
+								<a
+									className='no-underline'
+									onClick={() => router.push('/my-shows/plan-to-watch')}
+								>
+									Plan to Watch:{' '}
+								</a>
+								<span>
+									{
+										usersShowsData?.filter(
+											show => show.status === 'PLAN_TO_WATCH'
+										).length
+									}
+								</span>
+							</div>
+						</section>
+					</section>
+				</section>
 			</section>
 		</main>
 	);
