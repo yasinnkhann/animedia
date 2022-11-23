0fa75eb3bd2c78123309bc565a5f8013a0f3b925
Yasin Khan <yasinkhan500@gmail.com>
2022-11-22 18:25:48 -0800

    stache

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth, { type NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import FacebookProvider from 'next-auth/providers/facebook';
+import TwitterProvider from 'next-auth/providers/twitter';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { prisma } from '../../../lib/prisma';
@@ -12,6 +14,15 @@ export const authOptions: NextAuthOptions = {
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID as string,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+		}),
+		FacebookProvider({
+			clientId: process.env.FACEBOOK_CLIENT_ID as string,
+			clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+		}),
+		TwitterProvider({
+			clientId: process.env.TWITTER_CLIENT_ID as string,
+			clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
+			version: '2.0',
 		}),
 		CredentialsProvider({
 			name: 'Credentials',
