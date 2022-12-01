bf90c862bbe5341fb2df103d7ac0ad2fe085bd88
Yasin Khan <yasinkhan500@gmail.com>
2022-12-01 13:18:23 -0800

    fixed weird bug i think

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -91,13 +91,13 @@ export const authOptions: NextAuthOptions = {
 			// console.log('account IN JWT: ', account);
 			// console.log('IS NEW USER IN JWT CB: ', !!isNewUser);
 			if (user) {
-				token.jwt = user?.access_token;
+				token.jwt = (user as any)?.access_token;
 				token.user = user;
 				(token.user as any).provider = account?.provider;
 				// console.log('USER IN JWT CB: ', user);
 				// console.log('TOKEN IN JWT CB: ', token);
 
-				if (profile?.email_verified && user?.id) {
+				if ((profile as any)?.email_verified && user?.id) {
 					const isEmailVerifiedUpdated = await prisma.user.findUnique({
 						where: { id: user.id },
 						select: { emailVerified: true },
@@ -129,7 +129,7 @@ export const authOptions: NextAuthOptions = {
 				}
 			}
 
-			session.jwt = token.jwt;
+			(session as any).jwt = token.jwt;
 			(session as any).user = token.user;
 			// console.log('TOKEN IN SESSION CB: ', token);
 			// console.log('SESSION IN SESSION CB: ', session);
