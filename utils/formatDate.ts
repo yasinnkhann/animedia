9ada21bf855042946a480f7c3ae8f6b7f16bb8ba
Yasin Khan <yasinkhan500@gmail.com>
2023-01-06 01:34:55 -0800

    hello world console log

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,7 +1,5 @@
 import moment from 'moment';
 
 export const formatDate = (dateStr: string) => {
-	console.log('hello world');
-
-	return moment(dateStr).format('MMM Do, YYYY');
+	return momentd(dateStr).format('MMM Do, YYYY');
 };
