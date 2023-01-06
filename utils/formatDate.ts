aebbddb032566ccbcaa950a28ad267a9586af5ef
Yasin Khan <yasinkhan500@gmail.com>
2023-01-06 01:32:59 -0800

    test

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,5 +1,5 @@
 import moment from 'moment';
 
 export const formatDate = (dateStr: string) => {
-	return moment(dateStr).format('MMM Do, YYYY');
+	return momentd(dateStr).format('MMM Do, YYYY');
 };
