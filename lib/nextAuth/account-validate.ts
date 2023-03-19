08a3ed88fa13e775ec85b4d5c5d08b018397e00f
Yasin Khan <yasinkhan500@gmail.com>
2023-03-18 17:24:45 -0700

    stache

--- lib/nextAuth/account-validate.ts
+++ lib/nextAuth/account-validate.ts
@@ -1,21 +1,45 @@
 import { isValidEmail } from '../../utils/isValidEmail';
 import { ILogin, IRegister } from '@ts/interfaces';
 
+function validatePassword(password: string) {
+	const errors: string[] = [];
+
+	if (!password) {
+		errors.push('Password Required!');
+	} else if (password.trim().length < 8 || password.trim().length > 20) {
+		errors.push('Password must be between 8 and 20 characters long!');
+	} else if (password.includes(' ')) {
+		errors.push('Password cannot contain spaces!');
+	}
+
+	return errors;
+}
+
+function validateEmail(email: string) {
+	const errors: string[] = [];
+
+	if (!email) {
+		errors.push('Email Required!');
+	} else if (!isValidEmail(email)) {
+		errors.push('Invalid Email!');
+	}
+
+	return errors;
+}
+
 export default async function loginValidate(values: ILogin) {
 	const errors: Partial<ILogin> = {};
 
-	if (!values.email) {
-		errors.email = 'Email Required';
-	} else if (!isValidEmail(values.email)) {
-		errors.email = 'Invalid Email';
+	const emailErrors = validateEmail(values.email);
+
+	if (emailErrors.length > 0) {
+		errors.email = emailErrors[0];
 	}
 
-	if (!values.password) {
-		errors.password = 'Password Required';
-	} else if (values.password.length < 8 || values.password.length > 20) {
-		errors.password = 'Must be greater than 8 and less then 20 characters long';
-	} else if (values.password.includes(' ')) {
-		errors.password = 'Invalid Password';
+	const passwordErrors = validatePassword(values.password);
+
+	if (passwordErrors.length > 0) {
+		errors.password = passwordErrors[0];
 	}
 
 	return errors;
@@ -24,30 +48,32 @@ export default async function loginValidate(values: ILogin) {
 export function registerValidate(values: IRegister) {
 	const errors: Partial<IRegister> = {};
 
-	if (!values.name) {
-		errors.name = 'Name Required';
+	const nameErrors = values.name ? [] : ['Name Required!'];
+
+	if (nameErrors.length > 0) {
+		errors.name = nameErrors[0];
 	}
 
-	if (!values.email) {
-		errors.email = 'Email Required';
-	} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-		errors.email = 'Invalid Email';
+	const emailErrors = validateEmail(values.email);
+
+	if (emailErrors.length > 0) {
+		errors.email = emailErrors[0];
 	}
 
-	if (!values.password) {
-		errors.password = 'Password Required';
-	} else if (!isValidEmail(values.email)) {
-		errors.password = 'Must be greater then 8 and less then 20 characters long';
-	} else if (values.password.includes(' ')) {
-		errors.password = 'Invalid Password';
+	const passwordErrors = validatePassword(values.password);
+
+	if (passwordErrors.length > 0) {
+		errors.password = passwordErrors[0];
 	}
 
-	if (!values.confirmPassword) {
-		errors.confirmPassword = 'Please confirm your password';
-	} else if (values.password !== values.confirmPassword) {
-		errors.confirmPassword = 'Passwords Do Not Match!';
-	} else if (values.confirmPassword.includes(' ')) {
-		errors.confirmPassword = 'Invalid Confirm Password';
+	if (values.password && values.confirmPassword) {
+		if (values.password !== values.confirmPassword) {
+			errors.confirmPassword = 'Passwords Do Not Match!';
+		} else if (values.confirmPassword.includes(' ')) {
+			errors.confirmPassword = 'Invalid Confirm Password!';
+		}
+	} else {
+		errors.confirmPassword = 'Please confirm your password!';
 	}
 
 	return errors;
