57f27730443372e1cb801ef2a98c771d534170e0
Yasin Khan <yasinkhan500@gmail.com>
2023-07-27 22:38:48 -0700

    stashe

--- utils/getDetailsPageRoute.ts
+++ utils/getDetailsPageRoute.ts
@@ -1,18 +1,17 @@
 import { ESearchType } from '@ts/enums';
 import { CLIENT_BASE_URL } from '../utils/constants';
+import unidecode from 'unidecode';
 
 export const getDetailsPageRoute = (
 	mediaType: ESearchType,
 	id: number,
 	title: string
 ) => {
-	const cleanTitle = title
+	const cleanTitle = unidecode(title)
 		.toLowerCase()
 		.replace(/[^a-z0-9\/☆ -]/gi, '')
-		.replace(/[\/☆]/gi, ' ')
 		.replace(/\s+/g, '-')
 		.replace(/-{2,}/g, '-')
 		.trim();
-
 	return `${CLIENT_BASE_URL}/${mediaType}/${id}-${cleanTitle}`;
 };
