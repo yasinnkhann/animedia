fe930174fc9911b0450fb8c8d5b0a706774b2234
Yasin Khan <yasinkhan500@gmail.com>
2023-08-19 23:35:57 -0700

    stache

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,6 +1,9 @@
 import { builder } from './builder';
 import { applyMiddleware } from 'graphql-middleware';
 import { permissions } from './permissions';
+import path from 'path';
+import fs from 'fs';
+import { lexicographicSortSchema, printSchema } from 'graphql';
 
 import './models/User';
 import './models/Movie';
@@ -8,3 +11,10 @@ import './models/Show';
 import './models/Person';
 
 export const schema = applyMiddleware(builder.toSchema({}), permissions);
+
+const schemaAsString = printSchema(lexicographicSortSchema(schema));
+
+fs.writeFileSync(
+	path.join(process.cwd(), './graphql/generated/code-gen/schema.gql'),
+	schemaAsString
+);
