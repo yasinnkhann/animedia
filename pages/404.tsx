16bbac9d5d0bf25823530a87052f7d79efe3c861
Yasin Khan <yasinkhan500@gmail.com>
2023-11-03 22:52:29 -0700

    stashe

--- pages/404.tsx
+++ pages/404.tsx
@@ -9,11 +9,11 @@ const Custom404 = () => {
 			</Head>
 
 			<main>
-				<section className='relative my-[6.25rem] mx-auto block w-[80%] text-center'>
+				<section className='relative mx-auto my-[6.25rem] block w-[80%] text-center'>
 					<div className='relative z-[2] inline-block h-[15.625rem] text-[13.75rem] tracking-[1rem]'>
 						404
 					</div>
-					<hr className='z-[-10] my-0 mx-auto h-2 w-[26.25rem] border-t-4 border-solid border-white p-0 text-center text-white after:relative after:inline-block after:py-0 after:px-1 after:text-3xl after:content-[\u2022]' />
+					<hr className='z-[-10] mx-auto my-0 h-2 w-[26.25rem] border-t-4 border-solid border-white p-0 text-center text-white after:relative after:inline-block after:px-1 after:py-0 after:text-3xl after:content-[\u2022]' />
 					<div className='relative my-2 block text-center text-[4em] leading-[80%] tracking-[0.75rem]'>
 						THE PAGE
 					</div>
