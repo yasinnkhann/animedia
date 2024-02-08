2bd86fa268619af918757f97937158a4bfd2af13
Yasin Khan <yasinkhan500@gmail.com>
2024-02-07 23:21:33 -0800

    got rid of unstable method

--- lib/nextAuth/get-client-auth-session.ts
+++ lib/nextAuth/get-client-auth-session.ts
@@ -1,15 +1,11 @@
 import type { GetServerSideProps, GetServerSidePropsContext } from 'next';
-import { unstable_getServerSession } from 'next-auth';
+import { getServerSession } from 'next-auth';
 
 import { authOptions as nextAuthOptions } from '../../pages/api/auth/[...nextauth]';
 
 export const getClientAuthSession =
 	(func: GetServerSideProps) => async (ctx: GetServerSidePropsContext) => {
-		const session = await unstable_getServerSession(
-			ctx.req,
-			ctx.res,
-			nextAuthOptions
-		);
+		const session = await getServerSession(ctx.req, ctx.res, nextAuthOptions);
 
 		if (!session) {
 			return {
