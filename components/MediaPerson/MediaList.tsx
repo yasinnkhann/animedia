d368d18694bcf9cf659a1763ed3206b32bb06435
Yasin Khan <yasinkhan500@gmail.com>
2024-05-06 23:47:24 -0700

    stashe

--- components/MediaPerson/MediaList.tsx
+++ components/MediaPerson/MediaList.tsx
@@ -17,56 +17,69 @@ const MediaList = ({ mediaData, pageNum, title, genrePage }: Props) => {
 
 	return (
 		<section
-			className={`w-full ${
-				!genrePage ? 'px-4 sm:px-10 md:px-20 lg:px-40' : ''
-			}`}
+			className={`w-full ${!genrePage ? 'px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16 2xl:px-24' : ''}`}
 		>
 			<section className='flex flex-col'>
-				<h3 className='mb-2'>{title.split('_').join(' ')}</h3>
-				<table className='w-full'>
-					<thead>
-						<tr className='border-2 border-gray-200'>
-							<th className='w-[5rem] border-r-2 border-gray-200 p-4'>Rank</th>
-							<th className='border-r-2 border-gray-200 p-4'>Title</th>
-							<th className='w-[5rem] p-4'>Rating</th>
-							{session && (
-								<>
-									<th className='w-[7rem] border-x-2 border-gray-200 p-4'>
-										My Rating
-									</th>
-									<th className='w-[7rem] p-4'>Status</th>
-								</>
-							)}
-						</tr>
-					</thead>
+				<h3 className='mb-2 ml-4 text-lg lg:text-xl'>
+					{title.split('_').join(' ')}
+				</h3>
+				<div className='overflow-x-auto'>
+					<table className='w-full table-auto text-sm sm:text-base'>
+						<thead>
+							<tr className='border-2 border-gray-200'>
+								<th className='w-1/6 border-r-2 border-gray-200 px-2 py-2 lg:w-1/12 lg:px-4 lg:py-4'>
+									Rank
+								</th>
+								<th className='border-r-2 border-gray-200 px-2  py-2 lg:px-4 lg:py-4'>
+									Title
+								</th>
+								<th className='w-1/6 border-r-2 border-gray-200 px-2 py-2 lg:w-1/12 lg:px-4 lg:py-4'>
+									Rating
+								</th>
+								{session && (
+									<>
+										<th className='w-1/6 border-r-2 border-gray-200 px-2 py-2 lg:w-1/12 lg:px-4 lg:py-4'>
+											My Rating
+										</th>
+										<th className='w-1/6 px-2 py-2 lg:w-1/12 lg:px-4 lg:py-4'>
+											Status
+										</th>
+									</>
+								)}
+							</tr>
+						</thead>
+						<tbody className='divide-y divide-gray-200'>
+							{mediaData.results.map((media, idx) => {
+								let mediaComp;
 
-					<tbody className='!border-b-2 !border-gray-200'>
-						{mediaData.results.map((media, idx) => {
-							let mediaComp: JSX.Element;
-
-							if ('title' in media) {
-								mediaComp = (
-									<MovieCard
-										movie={media}
-										rank={
-											pageNum * RESULTS_PER_PAGE - (RESULTS_PER_PAGE - idx) + 1
-										}
-									/>
-								);
-							} else {
-								mediaComp = (
-									<ShowCard
-										show={media}
-										rank={
-											pageNum * RESULTS_PER_PAGE - (RESULTS_PER_PAGE - idx) + 1
-										}
-									/>
-								);
-							}
-							return <Fragment key={media.id}>{mediaComp}</Fragment>;
-						})}
-					</tbody>
-				</table>
+								if ('title' in media) {
+									mediaComp = (
+										<MovieCard
+											movie={media}
+											rank={
+												pageNum * RESULTS_PER_PAGE -
+												(RESULTS_PER_PAGE - idx) +
+												1
+											}
+										/>
+									);
+								} else {
+									mediaComp = (
+										<ShowCard
+											show={media}
+											rank={
+												pageNum * RESULTS_PER_PAGE -
+												(RESULTS_PER_PAGE - idx) +
+												1
+											}
+										/>
+									);
+								}
+								return <Fragment key={media.id}>{mediaComp}</Fragment>;
+							})}
+						</tbody>
+					</table>
+				</div>
 			</section>
 		</section>
 	);
