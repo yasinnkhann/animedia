a231aa05a3600809b60d05b20a18eac0b8bb1055
Yasin Khan <yasinkhan500@gmail.com>
2025-08-23 17:27:22 -0700

    added cron job test

---
+++ pages/api/fetchUsers.ts
@@ -0,0 +1,49 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { CLIENT_BASE_URL } from '../../utils/constants';
+import * as Queries from '../../graphql/queries';
+
+const USERS = `
+  query Users {
+    users {
+      id
+      name
+      email
+      emailVerified
+      image
+      password
+      created_at
+      movies {
+        id
+        name
+        status
+        rating
+      }
+      shows {
+        id
+        name
+        status
+        rating
+        current_episode
+      }
+    }
+  }
+`;
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	try {
+		const response = await fetch(`${CLIENT_BASE_URL}/api/graphql`, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ query: USERS }),
+		});
+
+		const data = await response.json();
+
+		const users = data.data.users;
+
+		res.status(200).json({ success: true, users });
+	} catch (err) {
+		console.error('Error fetching users:', err);
+		res.status(500).json({ success: false, error: 'Failed to fetch users' });
+	}
+}
--- pages/api/test.ts
+++
@@ -1,6 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-	console.log('Cron job executed at: ', new Date());
-	res.status(200).send('Cron job executed');
-}
