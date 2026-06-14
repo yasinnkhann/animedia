5873ad4264919ac14c86025c96b062ee1f0ce618
Yasin Khan <yasinkhan500@gmail.com>
2026-06-14 14:53:53 -0700

    stashe

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,5 +1,5 @@
 import Redis from 'ioredis';
-import { __prod__, REDIS_MB_THRESHOLD } from '@utils/constants';
+import { __prod__ } from '@utils/constants';
 import logger from './logger';
 
 export const redis = __prod__
@@ -12,31 +12,3 @@ export const redis = __prod__
 
 redis.on('connect', () => logger.info('Redis Connected'));
 redis.on('error', err => logger.error('Redis Error', { error: err }));
-
-async function monitorRedisMemory() {
-  try {
-    const info = await redis.info('memory');
-    const usedMemoryLine = info.split('\n').find(line => line.startsWith('used_memory:'));
-    if (!usedMemoryLine) return;
-
-    const usedBytes = parseInt(usedMemoryLine.split(':')[1], 10);
-    const usedMB = usedBytes / (1024 * 1024);
-
-    if (usedMB > REDIS_MB_THRESHOLD) {
-      logger.warn('Redis memory usage is high', { usedMB: usedMB.toFixed(2) });
-      try {
-        await redis.flushdb();
-        logger.info('Redis database flushed due to high memory usage');
-      } catch (flushErr) {
-        logger.error('Failed to flush Redis database', { error: flushErr });
-        return;
-      }
-    }
-  } catch (err) {
-    logger.error('Error monitoring Redis memory', { error: err });
-  }
-}
-
-if (__prod__) {
-  setInterval(monitorRedisMemory, 30 * 1000);
-}
