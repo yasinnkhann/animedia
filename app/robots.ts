f8c38d2f38dec130a165f82a8bb9c72f1f832405
Yasin Khan <yasinkhan500@gmail.com>
2026-09-05 22:13:09 -0700

    chore: update robots.txt to disallow all crawling

--- app/robots.ts
+++ app/robots.ts
@@ -5,8 +5,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
-      disallow: ['/api/'],
+      disallow: '/',
     },
     sitemap: `${CLIENT_BASE_URL}/sitemap.xml`,
   };
